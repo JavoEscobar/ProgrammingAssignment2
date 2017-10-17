feda249a31cbf4615db774480260c8a9e3e9fb41
Javier Escobar <javier.e.escobar@gmail.com>
2017-10-16 21:48:38 -0400

    Initial commit for the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions illustrates how to handle environment variables
 
-## Write a short comment describing this function
+## Create a list containing a set of functions to handle a matrix data
+## The set of functions are:
+## set(y) set the original matrix
+## get() returns the original matrix
+## setinverse(inverse) sets the inverse matrix
+## getinverse() returns the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Compute the inverse matrix for a square matrix caching the result the first
+## time it is computed
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function (x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if (!is.null(i)) {
+    message("getting cache data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
+
